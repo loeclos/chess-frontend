19d9458aa564430db2c3c695b31e49cf95439e5d
_v3 <116607327+marsianjohncarter@users.noreply.github.com>
2025-05-28 20:05:22 -0700

    Change api route

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -281,7 +281,7 @@ export default function Game({ initialColor = 'white' }: GameProps) {
     }, [pieces]);
 
     useEffect(() => {
-        const newSocket = io('https://chess-backend-lv8y.onrender.com/api', {
+        const newSocket = io('https://chess-backend-lv8y.onrender.com/', {
             transports: ["websocket"],
             withCredentials: true,
             reconnectionAttempts: 5,
